ac62a023715e2d8f0671c063503b70ebc1b9a405
RQLMIUEE <riquelme.ct.santos@gmail.com>
2026-08-24 12:00:09 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
-echo"Ola, vc executou essa aplicação em nuvem";
+// Definimos o ano de nascimento
+$ano_nascimento = 1995;
+
+// Pegamos o ano atual de forma automática
+$ano_atual = date("Y");
+
+// Calculamos a idade
+$idade = $ano_atual - $ano_nascimento;
+
+// Exibimos o resultado
+echo "Olá! Você tem $idade anos de idade.";
 ?>
